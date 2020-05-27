df5ec71cd3addae4f8c283b0170e8782db5035af
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2020-05-27 12:15:27 +0530

    Create Veriable.php

---
+++ Veriable.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "SOHAM";
+
+//1. Local Veriable
+//2. Global Veriable
+//3. Super Global Veriable
+
+$veriable01=$_POST["ver01"];
+$veriable02=$_POST["ver02"];
+$veriable03=$_POST["ver03"];
+print($veriable01.$veriable02.$veriable03)
+
+?>
